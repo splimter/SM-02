ef828d5b965928bb27cfc7842a27e550c68d027e
splimterXrodina <merah.soehyb@gmail.com>
2023-03-24 20:25:03 +0100

    add task 2

--- app/src/main/java/soheyb/merah/ipb_tp1/ListUsersActivity.kt
+++ app/src/main/java/soheyb/merah/ipb_tp1/ListUsersActivity.kt
@@ -1,11 +1,18 @@
 package soheyb.merah.ipb_tp1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ListUsersActivity : AppCompatActivity() {
+    lateinit var lblFullname: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_users)
+
+        lblFullname = findViewById(R.id.lblFullname)
+        val name: String? = intent.getStringExtra("name")
+        lblFullname.text = "Welcome "+name+" !"
     }
 }
--- app/src/main/java/soheyb/merah/ipb_tp1/MainActivity.kt
+++ app/src/main/java/soheyb/merah/ipb_tp1/MainActivity.kt
@@ -4,16 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnToListUsers: Button
+    lateinit var txtFullname: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnToListUsers = findViewById(R.id.btnToListUsers)
+        txtFullname = findViewById(R.id.txtFullname)
         btnToListUsers.setOnClickListener {
             intent = Intent(this, ListUsersActivity::class.java)
+            intent.putExtra("name", txtFullname.text.toString())
+            println(txtFullname.text)
             startActivity(intent)
         }
     }
